2f05ab373843499757f6a8f0709b16f269ed8f2b
SWARVY <swarvy0826@naver.com>
2023-04-06 11:40:15 +0900

    feat : 상단 네비바 항목 수정

--- proj_web/src/Components/NavbarComponent.jsx
+++ proj_web/src/Components/NavbarComponent.jsx
@@ -1,17 +1,18 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-
-const navigation = [
-  { name: '커뮤니티', href: '#', current: false },
-  { name: '컬렉션', href: '#', current: false },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import { Link } from 'react-router-dom';
 
 export default function NavbarComponent() {
+  const navigation = [
+    { name: '소개', href: '#', current: false },
+    { name: '커뮤니티', href: '#', current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -107,32 +108,32 @@ export default function NavbarComponent() {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             마이페이지
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             회원 탈퇴
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             로그아웃
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                     </Menu.Items>
