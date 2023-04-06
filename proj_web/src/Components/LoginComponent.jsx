1b6b5702732ec46bf3ed145f2906d8bcd03dc00f
SWARVY <swarvy0826@naver.com>
2023-04-06 14:47:26 +0900

    edit : 로그인페이지 버튼, 레이아웃 수정

--- proj_web/src/Components/LoginComponent.jsx
+++ proj_web/src/Components/LoginComponent.jsx
@@ -10,12 +10,14 @@ function LoginComponent() {
           </div>
           <div>
             <div className="text-4xl mb-4 text-center">LOGIN</div>
-            <div className="font-suiteLight mb-3">다음의 방법으로 로그인 할 수 있어요</div>
-            <div className="transition ease-in-out delay-100 font-suiteLight bg-kakao hover:bg-kakao_hover text-sm rounded-md p-2 mb-2 text-right shadow-md">
-              카카오계정으로 로그인
+            <div className="font-suiteLight mb-3 text-center">다음의 방법으로 로그인 할 수 있어요</div>
+            <div className="grid grid-cols-2 transition ease-in-out delay-100 font-suiteLight bg-kakao hover:bg-kakao_hover text-sm rounded-md p-2 mb-2 text-right shadow-md">
+              <img className="inline w-6 h-6" src={process.env.PUBLIC_URL + '/Images/kakao_logo.svg'} alt="kakao" />
+              <p className="flex items-center justify-end">카카오계정으로 로그인</p>
             </div>
-            <div className="transition ease-in-out delay-100 font-suiteLight bg-white hover:bg-white_hover  text-sm rounded-md p-2 text-right shadow-md">
-              구글계정으로 로그인
+            <div className="grid grid-cols-2 transition ease-in-out delay-100 font-suiteLight bg-white hover:bg-white_hover  text-sm rounded-md p-2 text-right shadow-md">
+              <img className="inline w-6 h-6" src={process.env.PUBLIC_URL + '/Images/google_logo.svg'} alt="kakao" />
+              <p className="flex items-center justify-end">구글계정으로 로그인</p>
             </div>
           </div>
         </div>
