30e51602d3ea9506ac128466357ba3316a4f09ce
gwansikk <seorit98@gmail.com>
2023-05-02 13:58:46 +0900

    A: 필터 버튼

---
+++ proj_web/src/Components/Discover/FilterButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FilterButton = ({ name }) => {
+  return <button className="bg-gd-blue hover:bg-gd-blue_hover text-white py-1 px-6 rounded-full font-bold">{name}</button>;
+};
+
+FilterButton.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default FilterButton;
