c1c88c700df370f3004c793acdfd96c93252493e
gwansikk <seorit98@gmail.com>
2023-05-29 20:06:44 +0900

    * Google Oauth

--- proj_web/src/Components/Gallery/GalleryCard.jsx
+++ proj_web/src/Components/Gallery/GalleryCard.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import MainImageModal from '../Modals/MainImageModal';
-import { useRecoilValue } from 'recoil';
-import { LoginState } from '../../Atoms/LoginState';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import MainImageModal from "../Modals/MainImageModal";
+import { useRecoilValue } from "recoil";
+import { LoginState } from "../../Atoms/LoginState";
 
 GalleryCard.propTypes = {
   url: PropTypes.string.isRequired,
@@ -35,7 +35,7 @@ export default function GalleryCard({ url, isMain }) {
           <Link to="/post">
             <img className="transition ease-in h-auto max-w-full rounded-md hover:opacity-70" src={url} alt="img" />
           </Link>
-          {buttonShow && true ? (
+          {buttonShow && isLogin ? (
             //{buttonShow && isLogin ? (작업용 로그인 고정
             <button className="absolute bottom-0 right-0 mb-2 mr-2 text-black px-2 py-2 rounded">
               <div className="flex">
@@ -49,27 +49,11 @@ export default function GalleryCard({ url, isMain }) {
                     className="transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:fill-red-500 duration-300 w-6 h-6 mr-1"
                     onClick={() => setLike(!like)}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                   </svg>
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="fill-red-500 duration-300 w-6 h-6 mr-1"
-                    onClick={() => setLike(!like)}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="fill-red-500 duration-300 w-6 h-6 mr-1" onClick={() => setLike(!like)}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                   </svg>
                 )}
                 {!bookMark ? (
@@ -82,27 +66,11 @@ export default function GalleryCard({ url, isMain }) {
                     className="transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:fill-ezip-green duration-300 w-6 h-6"
                     onClick={() => setBookMark(!bookMark)}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                    />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                   </svg>
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="fill-ezip-green duration-300 w-6 h-6"
-                    onClick={() => setBookMark(!bookMark)}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="fill-ezip-green duration-300 w-6 h-6" onClick={() => setBookMark(!bookMark)}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                   </svg>
                 )}
               </div>
@@ -113,12 +81,7 @@ export default function GalleryCard({ url, isMain }) {
         </>
       ) : (
         <>
-          <img
-            className="transition ease-in h-auto max-w-full rounded-md hover:scale-105"
-            src={url}
-            alt="img"
-            onClick={() => setModalShow(true)}
-          />
+          <img className="transition ease-in h-auto max-w-full rounded-md hover:scale-105" src={url} alt="img" onClick={() => setModalShow(true)} />
           <MainImageModal img={url} imgShow={modalShow} onClose={onImageHandler} />
         </>
       )}
--- proj_web/src/Components/Mypage/Mypage.jsx
+++ proj_web/src/Components/Mypage/Mypage.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import MypageButtons from './MypageGalleryButtons';
-import MainGalleryComponent from '../Gallery/MainGallery';
-import FollowerModal from './FollowModal';
-import FollowButton from './FollowButton';
-import { Avatar } from 'flowbite-react';
-import { useRecoilValue } from 'recoil';
-import { LoginState } from '../../Atoms/LoginState';
+import React, { useEffect, useState } from "react";
+import MypageButtons from "./MypageGalleryButtons";
+import MainGalleryComponent from "../Gallery/MainGallery";
+import FollowerModal from "./FollowModal";
+import FollowButton from "./FollowButton";
+import { Avatar } from "flowbite-react";
+import { useRecoilValue } from "recoil";
+import { LoginState } from "../../Atoms/LoginState";
 
 export default function Mypage() {
   const [followerShow, setFollowerShow] = useState(false);
@@ -16,20 +16,20 @@ export default function Mypage() {
     setFollowerShow(false);
   };
   const [followingDirection, setFollowingDirection] = useState(-1);
+
+  useEffect(() => {
+    setWhoami(true);
+  }, []);
+
   return (
     <div className="flex flex-col gap-y-5">
       <section className="flex flex-col items-center bg-white dark:bg-gray-900 antialiased  border-b">
         <div className="min-w-fit max-w-screen-xl px-4 py-5 lg:px-6 sm:py-5 lg:py-5">
           <div className="max-w-2xl mx-auto grid justify-items-center text-center gap-2">
-            <Avatar
-              className="rounded-full bg-gray-500 w-24 h-24 mb-2"
-              alt="User Profile"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded={true}
-            />
+            <Avatar className="rounded-full bg-gray-500 w-24 h-24 mb-2" alt="User Profile" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />
             <h2 className="text-2xl font-bold text-black sm:text-4xl dark:text-white">김관식</h2>
             <p className="text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">연무동 개발자</p>
-            {true && whoami ? (
+            {isLogin && whoami ? (
               // {isLogin && whoami ? 작업용 로그인 고정 + user확인
               <></>
             ) : (
