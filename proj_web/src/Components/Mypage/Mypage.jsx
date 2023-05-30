5f4e87f32c8892a6d3b3627305e9d64fb12ae0fd
ssangttuce <sanghoo1023@gmail.com>
2023-05-30 11:49:37 +0900

    add: 프로필 수정 버튼 추가

--- proj_web/src/Components/Mypage/Mypage.jsx
+++ proj_web/src/Components/Mypage/Mypage.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from "react";
-import MypageButtons from "./MypageGalleryButtons";
-import MainGalleryComponent from "../Gallery/MainGallery";
-import FollowerModal from "./FollowModal";
-import FollowButton from "./FollowButton";
-import { Avatar } from "flowbite-react";
-import { useRecoilValue } from "recoil";
-import { LoginState } from "../../Atoms/LoginState";
+import React, { useEffect, useState } from 'react';
+import MypageButtons from './MypageGalleryButtons';
+import MainGalleryComponent from '../Gallery/MainGallery';
+import FollowerModal from './FollowModal';
+import FollowButton from './FollowButton';
+import { Avatar, Button } from 'flowbite-react';
+import { useRecoilValue } from 'recoil';
+import { LoginState } from '../../Atoms/LoginState';
 
 export default function Mypage() {
-  const [followerShow, setFollowerShow] = useState(false);
+  const [followShow, setFollowShow] = useState(false);
+  const [followingDirection, setFollowingDirection] = useState(-1);
+  //follow list show & following or follower
   const [whoami, setWhoami] = useState(true);
   // user본인의 마이페이지인지 확인
   const isLogin = useRecoilValue(LoginState);
   const onFollowHandler = () => {
-    setFollowerShow(false);
+    setFollowShow(false);
   };
-  const [followingDirection, setFollowingDirection] = useState(-1);
 
   useEffect(() => {
-    setWhoami(true);
+    setWhoami(false);
   }, []);
 
   return (
@@ -26,15 +27,15 @@ export default function Mypage() {
       <section className="flex flex-col items-center bg-white dark:bg-gray-900 antialiased  border-b">
         <div className="min-w-fit max-w-screen-xl px-4 py-5 lg:px-6 sm:py-5 lg:py-5">
           <div className="max-w-2xl mx-auto grid justify-items-center text-center gap-2">
-            <Avatar className="rounded-full bg-gray-500 w-24 h-24 mb-2" alt="User Profile" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />
+            <Avatar
+              className="rounded-full bg-gray-500 w-24 h-24 mb-2"
+              alt="User Profile"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded={true}
+            />
             <h2 className="text-2xl font-bold text-black sm:text-4xl dark:text-white">김관식</h2>
             <p className="text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">연무동 개발자</p>
-            {isLogin && whoami ? (
-              // {isLogin && whoami ? 작업용 로그인 고정 + user확인
-              <></>
-            ) : (
-              <FollowButton></FollowButton>
-            )}
+            {isLogin && whoami ? <></> : <FollowButton></FollowButton>}
           </div>
 
           <div className="min-w-fit max-w-screen-xl flex justify-center mt-5 text-center gap-x-16 gap-y-5">
@@ -45,7 +46,7 @@ export default function Mypage() {
 
             <button
               onClick={() => {
-                setFollowerShow(true);
+                setFollowShow(true);
                 setFollowingDirection(0);
               }}
             >
@@ -57,7 +58,7 @@ export default function Mypage() {
 
             <button
               onClick={() => {
-                setFollowerShow(true);
+                setFollowShow(true);
                 setFollowingDirection(1);
               }}
             >
@@ -66,8 +67,14 @@ export default function Mypage() {
                 <p className="text-lg font-normal text-gray-500 dark:text-gray-400">Follower</p>
               </div>
             </button>
-            <FollowerModal followDirection={followingDirection} followerShow={followerShow} onClose={onFollowHandler} />
+            <FollowerModal followDirection={followingDirection} followerShow={followShow} onClose={onFollowHandler} />
           </div>
+          <Button
+            color="light"
+            className="mt-5 w-full h-7 ring-2 ring-gray-300 ring-inset hover:ring-2 hover:ring-inset"
+          >
+            <p>프로필 수정</p>
+          </Button>
         </div>
       </section>
 
