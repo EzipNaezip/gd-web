b01f3cd2abb3ed22eca848f68c4f6d4125105ea9
SWARVY <swarvy0826@naver.com>
2023-05-30 14:54:02 +0900

    feat : 생성 버튼 누를 시 로딩 인풋바 애니메이션 효과 추가, axios 라이브러리 설치, generating 상태 변경

--- proj_web/src/Components/Main/MainPrompt.jsx
+++ proj_web/src/Components/Main/MainPrompt.jsx
@@ -8,13 +8,19 @@ export default function MainPrompt() {
       {!generating ? (
         <h1 className="text-2xl font-bold text-center mb-4">새로운 아이디어를 찾아보세요!</h1>
       ) : (
-        <h1 className="text-2xl font-bold text-center mb-4">디자이너가 열심히 그리는 중...</h1>
+        <>
+          {generating === 'pending' ? (
+            <h1 className="text-2xl font-bold text-center mb-4">디자이너가 열심히 그리는 중...</h1>
+          ) : (
+            <h1 className="text-2xl font-bold text-center mb-4">당신의 아이디어가 완성되었습니다!</h1>
+          )}
+        </>
       )}
 
       <form>
         <div className="relative">
           {!generating ? (
-            <>
+            <div>
               <input
                 className="h-10 block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="원하시는 스타일을 입력해주세요."
@@ -23,30 +29,30 @@ export default function MainPrompt() {
               <button
                 onClick={(e) => {
                   e.preventDefault();
-                  setGenerating(true);
+                  setGenerating('pending');
                 }}
                 className="absolute right-0 bottom-0 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-r-lg border px-6 h-full border-gray-300"
               >
                 생성
               </button>
-            </>
+            </div>
           ) : (
-            <>
+            <div>
               <input
-                className="h-10 block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="animate-pulse delay-100 h-10 block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="원하시는 스타일을 입력해주세요."
                 disabled={true}
               />
               <button
                 onClick={(e) => {
                   e.preventDefault();
-                  setGenerating(false);
+                  setGenerating(null);
                 }}
                 className="absolute right-0 bottom-0 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-r-lg border px-6 h-full border-gray-300"
               >
                 중단
               </button>
-            </>
+            </div>
           )}
         </div>
       </form>
