bf200129d7b31c2681ae2308b76928676681b429
ssangttuce <sanghoo1023@gmail.com>
2023-05-31 16:08:41 +0900

    feat: follow button font color modified

--- proj_web/src/Components/Mypage/FollowButton.jsx
+++ proj_web/src/Components/Mypage/FollowButton.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Button } from "flowbite-react";
+import React, { useState } from 'react';
+import { Button } from 'flowbite-react';
 
 export default function FollowButton() {
   const [Followed, setFollowed] = useState(false);
@@ -7,25 +7,25 @@ export default function FollowButton() {
   // GalleryCardckarh: 사용자 상태를 참조하여 컴포넌트 상태 관리
   return (
     <div className="w-full">
-      {!Followed ? (
+      {Followed ? (
         <Button
           size="xs"
-          className="w-full px-1 bg-ezip-green text-ezip-bg  hover:bg-ezip-green_hover duration-75"
+          className="w-full px-1 bg-white ring-2 ring-inset ring-ezip-green hover:bg-ezip-bg hover:text-ezip-green_hover hover:ring-ezip-green_hover duration-75 "
+          onClick={() => {
+            setFollowed(false);
+          }}
+        >
+          <span className="text-base font-light text-ezip-green">Following</span>
+        </Button>
+      ) : (
+        <Button
+          size="xs"
+          className="w-full px-1 bg-ezip-green hover:bg-ezip-green_hover duration-75"
           onClick={() => {
             setFollowed(true);
           }}
         >
           <span className="text-base font-light">Follow</span>
-        </Button>
-      ) : (
-        <Button
-          size="xs"
-          className="w-full px-1 bg-ezip-bg text-ezip-green ring-1 ring-ezip-green hover:bg-ezip-bg hover:text-ezip-green_hover hover:ring-ezip-green_hover duration-75 "
-          onClick={() => {
-            setFollowed(false);
-          }}
-        >
-          <span className="text-base font-light">Following</span>
         </Button>
       )}
     </div>
