34a314912e38f37b453eee687b57b7a33bc07f85
gwansikk <seorit98@gmail.com>
2023-06-06 14:46:04 +0900

    A: 개인정보처리방침

--- proj_web/src/App.js
+++ proj_web/src/App.js
@@ -1,22 +1,23 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import MainPage from './Pages/MainPage';
-import Navigator from './Components/Navigator.jsx';
-import DiscoverPage from './Pages/DiscoverPage';
-import PostPage from './Pages/PostPage';
-import MypagePage from './Components/Mypage/MainMypage';
-import ScrollToTop from './Utilities/ScrollToTop';
-import React, { useEffect } from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { useRecoilValue } from 'recoil';
-import { PostNumState } from './Atoms/PostNumState';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import Navigator from "./Components/Navigator.jsx";
+import DiscoverPage from "./Pages/DiscoverPage";
+import PostPage from "./Pages/PostPage";
+import MypagePage from "./Components/Mypage/MainMypage";
+import ScrollToTop from "./Utilities/ScrollToTop";
+import React, { useEffect } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useRecoilValue } from "recoil";
+import { PostNumState } from "./Atoms/PostNumState";
+import PolicyPage from "./Pages/PolicyPage";
 
 export default function App() {
   const postNum = useRecoilValue(PostNumState);
 
   useEffect(() => {
     window.localStorage.setItem(
-      'test',
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b2tlbiIsImZvbGxvd0NvdW50IjoiMCIsInJvbGUiOiJBRE1JTiIsInByb3ZpZGVyIjoiR09PR0xFIiwicHJvZmlsZUltZ1VybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcm9maWxlLnBuZyIsIm5hbWUiOiJbMSA6IGFkbWluXSIsImRlc2NyaXB0aW9uIjoiVGhpcyBpcyB0aGUgYWRtaW4gdXNlciIsInBvc3RDb3VudCI6IjAiLCJ1c2VySWQiOiIxIiwiZm9sbG93ZXJDb3VudCI6IjAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwiaWF0IjoxNjg1NTMzMDY4LCJleHAiOjE3MTcxNTU0Njh9.rXmhh-RDdnXszwUKa-G7TsuB1zoa1M7CABwUwY8VxOU',
+      "test",
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b2tlbiIsImZvbGxvd0NvdW50IjoiMCIsInJvbGUiOiJBRE1JTiIsInByb3ZpZGVyIjoiR09PR0xFIiwicHJvZmlsZUltZ1VybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcm9maWxlLnBuZyIsIm5hbWUiOiJbMSA6IGFkbWluXSIsImRlc2NyaXB0aW9uIjoiVGhpcyBpcyB0aGUgYWRtaW4gdXNlciIsInBvc3RDb3VudCI6IjAiLCJ1c2VySWQiOiIxIiwiZm9sbG93ZXJDb3VudCI6IjAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwiaWF0IjoxNjg1NTMzMDY4LCJleHAiOjE3MTcxNTU0Njh9.rXmhh-RDdnXszwUKa-G7TsuB1zoa1M7CABwUwY8VxOU"
     );
   }, []);
 
@@ -31,6 +32,7 @@ export default function App() {
             <Route path="/discover" element={<DiscoverPage />} />
             <Route path={`/post/${postNum}`} element={<PostPage />} />
             <Route path="/mypage" element={<MypagePage />} />
+            <Route path="/policy" element={<PolicyPage />} />
           </Routes>
         </section>
       </BrowserRouter>
