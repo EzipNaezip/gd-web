aa78074a73c50d3a9ec74f025968b4cd70718416
SWARVY <swarvy0826@naver.com>
2023-06-07 19:07:34 +0900

    fix : Carousel 버그 수정중 테스트 9

--- proj_web/src/Components/Post/PostCarousel.jsx
+++ proj_web/src/Components/Post/PostCarousel.jsx
@@ -5,10 +5,12 @@ export default function PostCarousel({ images, onImageHandler }) {
   const baseURL = 'http://api.ezipnaezip.life:8080';
 
   return (
-    <Carousel className="h-auto lg:h-[30rem]">
-      {images.map((path) => (
-        <img onClick={onImageHandler} src={`${baseURL}${path}`} alt="..." />
-      ))}
-    </Carousel>
+    <div className="h-80 lg:h-[30rem]">
+      <Carousel>
+        {images.map((path) => (
+          <img onClick={onImageHandler} src={`${baseURL}${path}`} alt="..." />
+        ))}
+      </Carousel>
+    </div>
   );
 }
