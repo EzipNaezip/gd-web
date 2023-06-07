c895a06c5f76c4f78a415ce3966a35f68130dbf3
SWARVY <swarvy0826@naver.com>
2023-06-07 18:41:33 +0900

    fix : Carousel 버그 수정중 테스트 5

--- proj_web/src/Components/Post/PostCarousel.jsx
+++ proj_web/src/Components/Post/PostCarousel.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Carousel } from 'flowbite';
+import { Carousel } from 'flowbite-react';
 
 export default function PostCarousel({ images, onImageHandler }) {
   const baseURL = 'http://api.ezipnaezip.life:8080';
 
   return (
     <>
-      <Carousel>
-        {images.map((path) => {
-          return <img onClick={onImageHandler} src={`${baseURL}${path}`} alt="..." />;
-        })}
+      <Carousel className="w-full">
+        {images.map((path) => (
+          <img onClick={onImageHandler} src={`${baseURL}${path}`} alt="..." />
+        ))}
       </Carousel>
     </>
   );
