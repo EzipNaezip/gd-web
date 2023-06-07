01f5d2b06905d7b2cd883a238065c0a46ad630e1
gwansikk <seorit98@gmail.com>
2023-06-07 13:53:45 +0900

    A: SweetAlert

--- proj_web/src/Pages/LoginTokenPage.jsx
+++ proj_web/src/Pages/LoginTokenPage.jsx
@@ -1,10 +1,23 @@
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import Swal from "sweetalert2";
 
 const LoginTokenPage = () => {
   const { token } = useParams();
   const navigate = useNavigate();
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "bottom-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
 
   useEffect(() => {
     // 정상적으로 토큰이 들어오면 세션 스토리지에 저장
@@ -13,9 +26,23 @@ const LoginTokenPage = () => {
       const userInfoJson = JSON.stringify(userInfo);
       const parsedUserInfo = JSON.parse(userInfoJson);
 
+      if (parsedUserInfo.userId) {
+        Toast.fire({
+          icon: "error",
+          title: "로그인 실패! 다시시도 해주세요.",
+        });
+
+        navigate("/");
+        return;
+      }
+
       sessionStorage.setItem("token", token);
       sessionStorage.setItem("userId", parsedUserInfo.userId);
 
+      Toast.fire({
+        icon: "success",
+        title: "로그인 성공! 환영합니다.",
+      });
       navigate("/");
     } else {
       console.log(`Invalid`);
