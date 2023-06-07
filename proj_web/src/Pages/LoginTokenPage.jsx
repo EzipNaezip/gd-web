01ae7aafe3c2e2d9684b670e9f8dec88c48eced6
gwansikk <seorit98@gmail.com>
2023-06-07 13:41:24 +0900

    F: 로그인 후 리다이렉션

--- proj_web/src/Pages/LoginTokenPage.jsx
+++ proj_web/src/Pages/LoginTokenPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 const LoginTokenPage = () => {
@@ -9,8 +9,10 @@ const LoginTokenPage = () => {
   // 정상적으로 토큰이 들어오면 세션 스토리지에 저장
   if (token) {
     const userInfo = jwt_decode(token); // 토큰 decode
+    const userInfoJson = JSON.stringify(userInfo);
+
     sessionStorage.setItem("token", token);
-    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    sessionStorage.setItem("userId", userInfoJson.userId);
 
     navigate("/");
   } else {
--- proj_web/src/Pages/MainPage.jsx
+++ proj_web/src/Pages/MainPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import MainPrompt from '../Components/Main/MainPrompt';
-import ScrollToDiscover from '../Components/Main/ScrollToDiscover';
-import { useRecoilValue } from 'recoil';
-import { PromptCreateState } from '../Atoms/PromptCreateState';
-import { useQuery } from 'react-query';
-import { getExample } from '../Query/MainExampleQuery';
-import MainImageGrid from '../Components/Main/MainImageGrid';
+import React from "react";
+import MainPrompt from "../Components/Main/MainPrompt";
+import ScrollToDiscover from "../Components/Main/ScrollToDiscover";
+import { useRecoilValue } from "recoil";
+import { PromptCreateState } from "../Atoms/PromptCreateState";
+import { useQuery } from "react-query";
+import { getExample } from "../Query/MainExampleQuery";
+import MainImageGrid from "../Components/Main/MainImageGrid";
 
 function MainPage() {
   const isCreated = useRecoilValue(PromptCreateState);
-  const getImages = useQuery('getExample', getExample, {
+  const getImages = useQuery("getExample", getExample, {
     refetchOnWindowFocus: false,
     retry: 0,
     onSuccess: (data) => {
-      console.log('example : ', data);
+      console.log("example : ", data);
     },
   });
 
