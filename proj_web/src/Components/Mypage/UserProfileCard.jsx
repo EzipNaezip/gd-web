8bde2e66732d8b6817230f2fd45839ce52c2a72e
SWARVY <swarvy0826@naver.com>
2023-06-09 23:48:50 +0900

    fix : 팔로잉 디자인 수정

--- proj_web/src/Components/Mypage/UserProfileCard.jsx
+++ proj_web/src/Components/Mypage/UserProfileCard.jsx
@@ -23,7 +23,7 @@ export default function UserProfileCard({ user, followUser, unfollowUser }) {
               <h2 className="text-sm font-suiteMedium text-black sm:text-base">{user.name}</h2>
             </div>
           </div>
-          <div className="w-24">
+          <div className="w-24 items-center">
             <FollowButton state={user.follow} user={user} followUser={followUser} unfollowUser={unfollowUser} />
           </div>
         </div>
