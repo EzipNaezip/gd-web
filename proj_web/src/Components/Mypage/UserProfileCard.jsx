535b67654cb4222c565ce2d89b07beef212c0cb2
SWARVY <swarvy0826@naver.com>
2023-06-09 23:44:10 +0900

    fix : 프로필 이미지 경로 판단

--- proj_web/src/Components/Mypage/UserProfileCard.jsx
+++ proj_web/src/Components/Mypage/UserProfileCard.jsx
@@ -5,12 +5,20 @@ import FollowButton from './FollowButton';
 export default function UserProfileCard({ user, followUser, unfollowUser }) {
   const baseURL = 'http://api.ezipnaezip.life:8080';
 
+  const checkImgURL = () => {
+    const regExpHttp = /http:/g;
+    const regExpHttps = /https:/g;
+
+    if (regExpHttp.test(user.profileImgUrl) || regExpHttps.test(user.profileImgUrl)) return user.profileImgUrl;
+    else return `${baseURL}${user.profileImgUrl}`;
+  };
+
   return (
     <>
       <ListGroup.Item>
         <div className="flex justify-between w-full">
           <div className="flex justify-start">
-            <Avatar className="mr-3" alt="User settings" img={`${baseURL}${user.profileImgUrl}`} rounded={true} />
+            <Avatar className="mr-3" alt="User settings" img={checkImgURL()} rounded={true} />
             <div className="grid justify-items-start">
               <h2 className="text-sm font-medium text-black sm:text-base">{user.name}</h2>
             </div>
