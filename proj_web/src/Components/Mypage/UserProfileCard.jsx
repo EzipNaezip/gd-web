57a95bcb19dc3bb04e21dba5475c8b2408f9148f
SWARVY <swarvy0826@naver.com>
2023-06-09 23:45:57 +0900

    fix : 팔로잉 디자인 수정

--- proj_web/src/Components/Mypage/UserProfileCard.jsx
+++ proj_web/src/Components/Mypage/UserProfileCard.jsx
@@ -19,8 +19,8 @@ export default function UserProfileCard({ user, followUser, unfollowUser }) {
         <div className="flex justify-between w-full">
           <div className="flex justify-start">
             <Avatar className="mr-3" alt="User settings" img={checkImgURL()} rounded={true} />
-            <div className="grid justify-items-start">
-              <h2 className="text-sm font-medium text-black sm:text-base">{user.name}</h2>
+            <div className="flex items-center justify-center">
+              <h2 className="text-sm font-suiteMedium text-black sm:text-base">{user.name}</h2>
             </div>
           </div>
           <div className="w-24">
