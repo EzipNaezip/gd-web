4406f19f38a85e077dcdb8fd11dd63328c8e172c
SWARVY <swarvy0826@naver.com>
2023-06-09 20:18:49 +0900

    fix : 팔로우 버튼 디버깅 6

--- proj_web/src/Components/Mypage/MainMypage.jsx
+++ proj_web/src/Components/Mypage/MainMypage.jsx
@@ -115,7 +115,7 @@ export default function MainMypage({ data, fetch, follow, following, followUser,
       </section>
       <MypageGalleryButtons />
       <div className="container">
-        <DiscoverImageGrid thumbnails={data.data.myPosts} />
+        <DiscoverImageGrid thumbnails={data.myPosts} />
       </div>
     </div>
   );
