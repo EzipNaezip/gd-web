4fec1bb6303603871710e863603629069224f018
SWARVY <swarvy0826@naver.com>
2023-06-12 19:59:50 +0900

    feat : 회원탈퇴 모달 구현

--- proj_web/src/Components/Mypage/ProfileSettingModal.jsx
+++ proj_web/src/Components/Mypage/ProfileSettingModal.jsx
@@ -3,12 +3,14 @@ import { Modal, Button } from 'flowbite-react';
 import { useMutation } from 'react-query';
 import { editUserInfo, upload } from '../../Query/MypageQuery';
 import { useParams } from 'react-router-dom';
+import WithdrawModal from './WithdrawModal';
 
 export default function ProfileSettingModal({ user, profileShow, fetch, setApiCall, onClose }) {
   const [imgFile, setImgFile] = useState(null);
   const [imgURL, setImgURL] = useState('');
   const [returnURL, setReturnURL] = useState('');
   const [uploadState, setUploadState] = useState(false);
+  const [withdrawShow, setwithdrawShow] = useState(false);
   const nameRef = useRef('');
   const descriptionRef = useRef('');
   const fileInput = useRef(null);
@@ -71,97 +73,103 @@ export default function ProfileSettingModal({ user, profileShow, fetch, setApiCa
   };
 
   return (
-    <Modal
-      className="h-screen animate-fade-in-down"
-      dismissible
-      size="md"
-      popup={true}
-      show={profileShow}
-      onClose={() => {
-        setImgURL(null);
-        nameRef.current.value = user.name;
-        descriptionRef.current.value = user.description;
-        setUploadState(false);
-        onClose();
-      }}
-    >
-      <Modal.Header>
-        <h1 className="pl-4 font-suiteBold">프로필 수정</h1>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="flex flex-col items-center w-full">
-          <form className="flex-col justify-center items-center">
-            {!uploadState ? (
-              <img
-                className="border rounded-full w-48 h-48 mb-2"
-                src={checkImgURL()}
-                alt="profile"
-                onClick={() => {
-                  fileInput.current.click();
-                }}
-              />
-            ) : (
-              <img
-                className="border rounded-full w-48 h-48 mb-2"
-                src={imgURL}
-                alt="profile"
-                onClick={() => {
-                  fileInput.current.click();
-                }}
-              />
-            )}
-            <input type="file" ref={fileInput} onChange={handleFileImage} style={{ display: 'none' }} />
-            <button
-              className="w-full items-center text-sm font-suiteLight"
-              onClick={(e) => {
-                e.preventDefault();
-                uploadFile.mutate(imgFile);
-              }}
-            >
-              이미지 업로드
-            </button>
-          </form>
-          <form className="mt-6 w-full">
-            <div className="flex flex-col items-center gap-2 w-full">
-              <div className="flex justify-center items-center w-full">
-                <h3 className="font-suiteLight text-sm w-1/6 mr-3">이름</h3>
-                <input
-                  type="text"
-                  name="username"
-                  placeholder="사용자 이름을 입력하세요."
-                  className="h-10 block w-3/4 p-4 text-sm font-suiteLight text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  defaultValue={user.name}
-                  ref={nameRef}
+    <>
+      <WithdrawModal show={withdrawShow} setShow={setwithdrawShow} />
+      <Modal
+        className="h-screen animate-fade-in-down"
+        dismissible
+        size="md"
+        popup={true}
+        show={profileShow}
+        onClose={() => {
+          setImgURL(null);
+          nameRef.current.value = user.name;
+          descriptionRef.current.value = user.description;
+          setUploadState(false);
+          onClose();
+        }}
+      >
+        <Modal.Header>
+          <h1 className="pl-4 font-suiteBold">프로필 수정</h1>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="flex flex-col items-center w-full">
+            <form className="flex-col justify-center items-center">
+              {!uploadState ? (
+                <img
+                  className="border rounded-full w-48 h-48 mb-2"
+                  src={checkImgURL()}
+                  alt="profile"
+                  onClick={() => {
+                    fileInput.current.click();
+                  }}
                 />
-              </div>
-              <div className="flex justify-center items-center w-full">
-                <h3 className="font-suiteLight text-sm w-1/6 mr-3">소개</h3>
-                <input
-                  type="text"
-                  name="userDescription"
-                  placeholder="당신을 소개하세요."
-                  className="h-10 block w-3/4 p-4 text-sm font-suiteLight text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  defaultValue={user.description}
-                  ref={descriptionRef}
+              ) : (
+                <img
+                  className="border rounded-full w-48 h-48 mb-2"
+                  src={imgURL}
+                  alt="profile"
+                  onClick={() => {
+                    fileInput.current.click();
+                  }}
                 />
-              </div>
-              <Button
-                className="mt-6 bg-ezip-green hover:bg-ezip-green_hover w-full"
+              )}
+              <input type="file" ref={fileInput} onChange={handleFileImage} style={{ display: 'none' }} />
+              <button
+                className="w-full items-center text-sm font-suiteLight"
                 onClick={(e) => {
                   e.preventDefault();
-                  handleSave();
+                  uploadFile.mutate(imgFile);
                 }}
               >
-                저장하기
-              </Button>
-            </div>
-            <hr className="mt-6" />
-            <p className="mt-6 w-full flex items-center justify-center text-xs font-suiteMedium text-red-500">
-              회원탈퇴
-            </p>
-          </form>
-        </div>
-      </Modal.Body>
-    </Modal>
+                이미지 업로드
+              </button>
+            </form>
+            <form className="mt-6 w-full">
+              <div className="flex flex-col items-center gap-2 w-full">
+                <div className="flex justify-center items-center w-full">
+                  <h3 className="font-suiteLight text-sm w-1/6 mr-3">이름</h3>
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="사용자 이름을 입력하세요."
+                    className="h-10 block w-3/4 p-4 text-sm font-suiteLight text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    defaultValue={user.name}
+                    ref={nameRef}
+                  />
+                </div>
+                <div className="flex justify-center items-center w-full">
+                  <h3 className="font-suiteLight text-sm w-1/6 mr-3">소개</h3>
+                  <input
+                    type="text"
+                    name="userDescription"
+                    placeholder="당신을 소개하세요."
+                    className="h-10 block w-3/4 p-4 text-sm font-suiteLight text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    defaultValue={user.description}
+                    ref={descriptionRef}
+                  />
+                </div>
+                <Button
+                  className="mt-6 bg-ezip-green hover:bg-ezip-green_hover w-full"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSave();
+                  }}
+                >
+                  저장하기
+                </Button>
+              </div>
+              <hr className="mt-6" />
+              <p
+                className="mt-6 w-full flex items-center justify-center text-xs font-suiteMedium text-red-500"
+                onClick={() => setwithdrawShow(true)}
+              >
+                회원탈퇴
+              </p>
+            </form>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
---
+++ proj_web/src/Components/Mypage/WithdrawModal.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, Modal } from 'flowbite-react';
+
+export default function WithdrawModal({ show, setShow }) {
+  return (
+    <Modal>
+      <Modal show={show} size="md" popup onClose={setShow(false)}>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <svg
+              className="mx-auto mb-4 h-14 w-14 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">정말로 탈퇴하시겠습니까?</h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={setShow(false)}>
+                탈퇴하기
+              </Button>
+              <Button color="gray" onClick={setShow(false)}>
+                취소하기
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </Modal>
+  );
+}
