ae26305d13117207539ed49229b4e02e54f27957
SWARVY <swarvy0826@naver.com>
2023-06-12 19:01:42 +0900

    fix : search 수정

--- proj_web/src/Components/Navigator.jsx
+++ proj_web/src/Components/Navigator.jsx
@@ -109,38 +109,48 @@ export default function Navigator() {
             발견
           </Navbar.Link>
         </Navbar.Collapse>
-        <div>
-          <form>
-            <label
-              htmlFor="default-search"
-              className="mb-2 text-sm font-suiteMedium text-gray-900 sr-only dark:text-white"
-            >
-              Search
+        <div className="hidden md:block w-1/3 mr-5">
+          <form className="flex items-center">
+            <label htmlFor="simple-search" className="sr-only">
+              검색
             </label>
-            <div className="relative">
+            <div className="relative w-full">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path
-                    fillRule="evenodd"
+                    fill-rule="evenodd"
                     d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
-                    clipRule="evenodd"
+                    clip-rule="evenodd"
                   />
                 </svg>
               </div>
               <input
-                type="search"
-                id="default-search"
-                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="키워드를 입력해주세요"
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 font-suiteMedium text-sm rounded-lg focus:ring-ezip-green focus:border-ezip-green block w-full pl-10 p-2.5"
+                placeholder="키워드를 입력하세요"
                 required
               />
-              <button
-                type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                검색
-              </button>
             </div>
+            <button
+              type="submit"
+              className="transition ease-in p-2.5 ml-2 text-sm font-suiteMedium text-white bg-ezip-green rounded-lg border border-ezip-green hover:bg-ezip-green_hover hover:border-ezip-green_hover focus:ring-ezip-green "
+            >
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+              <span className="sr-only">검색</span>
+            </button>
           </form>
         </div>
       </Navbar>
