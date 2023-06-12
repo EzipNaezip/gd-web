fd0b0dc006c03ae8bb029388ac9ec658963dfe8a
SWARVY <swarvy0826@naver.com>
2023-06-12 20:01:23 +0900

    fix : 회원탈퇴 모달 수정

--- proj_web/src/Components/Mypage/ProfileSettingModal.jsx
+++ proj_web/src/Components/Mypage/ProfileSettingModal.jsx
@@ -162,7 +162,10 @@ export default function ProfileSettingModal({ user, profileShow, fetch, setApiCa
               <hr className="mt-6" />
               <p
                 className="mt-6 w-full flex items-center justify-center text-xs font-suiteMedium text-red-500"
-                onClick={() => setwithdrawShow(true)}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setwithdrawShow(true);
+                }}
               >
                 회원탈퇴
               </p>
