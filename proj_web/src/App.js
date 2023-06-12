880ef8901f827dab15efdebc37b6853890937f70
SWARVY <swarvy0826@naver.com>
2023-06-12 17:32:23 +0900

    fix : navigator input태그 수정

--- proj_web/src/App.js
+++ proj_web/src/App.js
@@ -1,14 +1,15 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import MainPage from "./Pages/MainPage";
-import Navigator from "./Components/Navigator.jsx";
-import DiscoverPage from "./Pages/DiscoverPage";
-import PostPage from "./Pages/PostPage";
-import MypagePage from "./Pages/MypagePage";
-import PolicyPage from "./Pages/PolicyPage";
-import ScrollToTop from "./Utilities/ScrollToTop";
-import React, { useEffect, useState } from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import LoginTokenPage from "./Pages/LoginTokenPage";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import MainPage from './Pages/MainPage';
+import Navigator from './Components/Navigator.jsx';
+import DiscoverPage from './Pages/DiscoverPage';
+import PostPage from './Pages/PostPage';
+import MypagePage from './Pages/MypagePage';
+import PolicyPage from './Pages/PolicyPage';
+import ScrollToTop from './Utilities/ScrollToTop';
+import React, { useEffect, useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import LoginTokenPage from './Pages/LoginTokenPage';
+import SearchPage from './Pages/SearchPage';
 
 export default function App() {
   const [apiCall, setApiCall] = useState(false);
@@ -20,10 +21,10 @@ export default function App() {
 
   useEffect(() => {
     window.localStorage.setItem(
-      "test",
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b2tlbiIsImZvbGxvd0NvdW50IjoiMCIsInJvbGUiOiJBRE1JTiIsInByb3ZpZGVyIjoiR09PR0xFIiwicHJvZmlsZUltZ1VybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcm9maWxlLnBuZyIsIm5hbWUiOiJbMSA6IGFkbWluXSIsImRlc2NyaXB0aW9uIjoiVGhpcyBpcyB0aGUgYWRtaW4gdXNlciIsInBvc3RDb3VudCI6IjAiLCJ1c2VySWQiOiIxIiwiZm9sbG93ZXJDb3VudCI6IjAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwiaWF0IjoxNjg1NTMzMDY4LCJleHAiOjE3MTcxNTU0Njh9.rXmhh-RDdnXszwUKa-G7TsuB1zoa1M7CABwUwY8VxOU"
+      'test',
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b2tlbiIsImZvbGxvd0NvdW50IjoiMCIsInJvbGUiOiJBRE1JTiIsInByb3ZpZGVyIjoiR09PR0xFIiwicHJvZmlsZUltZ1VybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcm9maWxlLnBuZyIsIm5hbWUiOiJbMSA6IGFkbWluXSIsImRlc2NyaXB0aW9uIjoiVGhpcyBpcyB0aGUgYWRtaW4gdXNlciIsInBvc3RDb3VudCI6IjAiLCJ1c2VySWQiOiIxIiwiZm9sbG93ZXJDb3VudCI6IjAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwiaWF0IjoxNjg1NTMzMDY4LCJleHAiOjE3MTcxNTU0Njh9.rXmhh-RDdnXszwUKa-G7TsuB1zoa1M7CABwUwY8VxOU',
     );
-    window.localStorage.setItem("testID", "1");
+    window.localStorage.setItem('testID', '1');
     setRefreshNavigator(true);
   }, []);
 
@@ -41,7 +42,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/discover" element={<DiscoverPage />} />
-            <Route path="/discover/:keyword" element={} />
+            <Route path="/discover/:keyword" element={<SearchPage />} />
             <Route path="/post/:postNum" element={<PostPage />} />
             <Route path="/mypage/:userId" element={<MypagePage setApiCall={setCall} />} />
             <Route path="/policy" element={<PolicyPage />} />
