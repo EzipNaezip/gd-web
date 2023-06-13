05ebfd8347e595aaed98a533e3e6126ac0d1239e
SWARVY <swarvy0826@naver.com>
2023-06-14 00:24:04 +0900

    fix : filterbutton setCursor 버그 수정 2

--- proj_web/src/Components/Discover/DiscoverButtons.jsx
+++ proj_web/src/Components/Discover/DiscoverButtons.jsx
@@ -3,23 +3,29 @@ import DiscoverFilterButton from './DiscoverFilterButton';
 import { DiscoverFilterList } from './DiscoverFilterList';
 
 export default function DiscoverButtons({ cursor, setCursor, topHandler, filterHandler }) {
+  const handleTopClick = (e) => {
+    e.preventDefault();
+    setCursor(e.target.innerText);
+    topHandler();
+  };
+
+  const handleFilterClick = (e) => {
+    e.preventDefault();
+    const newCursor = e.target.innerText;
+    filterHandler(newCursor);
+    setCursor(newCursor);
+  };
+
   return (
     <div className="flex justify-center w-full">
       <div className="grid grid-cols-4 md:grid-cols-6 items-center justify-items-center lg:flex gap-4">
         {[...DiscoverFilterList].map((filter) => (
           <DiscoverFilterButton
+            key={filter}
             data={filter}
             cursor={cursor}
-            topHandler={(e) => {
-              e.preventDefault();
-              topHandler();
-              setCursor(e.target.innerText);
-            }}
-            filterHandler={(e) => {
-              e.preventDefault();
-              filterHandler(e.target.innerText);
-              setCursor(e.target.innerText);
-            }}
+            topHandler={handleTopClick}
+            filterHandler={handleFilterClick}
           />
         ))}
       </div>
